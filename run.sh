3129311730ffa7feaaefc70a8c991ed658d2fe90
Nigel Deakin <nigel.deakin@oracle.com>
2018-09-26 16:38:52 +0100

    Pro

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
+env
 echo "prop1:"
 env | grep ECHO-PROPERTIES-PROP1
 echo "prop2:"
